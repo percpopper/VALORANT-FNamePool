7a1d524b7ada9b43b90d09ad6560d2935d12104a
NT <89083789+N-T33@users.noreply.github.com>
2022-08-10 08:21:05 -0400

    VALORANT UPDATE - 5.03

--- VALORANT/VALORANT.h
+++ VALORANT/VALORANT.h
@@ -11,8 +11,8 @@ class FNamePool* NamePoolData = nullptr;
 
 #define FileName "\\FNamePool_Entries_Log.txt"
 
-#define DecryptWideOffset 0x2DBF0A0
-#define DecryptNonWideOffset 0x2DBF030 
-#define NamePoolDataOffset 0x867DC40 
+#define DecryptWideOffset 0x3014280
+#define DecryptNonWideOffset 0x30142F0 
+#define NamePoolDataOffset 0x8C3FF80 
 
 #include <VALORANT/FNamePool.h>
