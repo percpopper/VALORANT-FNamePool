6b2c414e128b0f62f4c1528187a1ed04cc84f359
PERC POPPER <89083789+percpopper@users.noreply.github.com>
2022-08-23 13:05:00 -0400

    5.04

--- VALORANT/VALORANT.h
+++ VALORANT/VALORANT.h
@@ -11,8 +11,8 @@ class FNamePool* NamePoolData = nullptr;
 
 #define FileName "\\FNamePool_Entries_Log.txt"
 
-#define DecryptWideOffset 0x3014280
-#define DecryptNonWideOffset 0x30142F0 
-#define NamePoolDataOffset 0x8C3FF80 
+#define DecryptWideOffset 0x30603A0
+#define DecryptNonWideOffset 0x3060330 
+#define NamePoolDataOffset 0x8CABF80
 
 #include <VALORANT/FNamePool.h>
